cde262cd104535508001d5c3b572f8d603333d71
Munish <munishmahey2@gmail.com>
2020-06-01 05:40:15 -0400

    custom error page changed

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,30 +1,18 @@
 import React from 'react'
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 
 function Error() {
     return (
-
-        <Card >
-      <CardContent>
-          Page Not Found (Status 400)
-      </CardContent>
-      <CardActions>
-        <Link to="/"><Button size="small">Back Home</Button></Link>
-      </CardActions>
-    </Card>
-        // <div>
-        //     <div className="ui placeholder segment">
-        //     <div className="ui icon header">
-        //         <i className="bug icon"></i>
-        //         Page Not Found (Status 404)
-        //     </div>
-        //     <Link to="/"><button style={{color:"blue"}}>Return Home</button></Link>
-        //     </div>
-        // </div>
+        <div className="banner-error">
+          <h1>Page not Found (Status 404)</h1>
+          <hr/>
+          <Link to="/">
+            <Button variant="contained" color="secondary">
+              Back to Home Page
+            </Button>
+          </Link>
+        </div>
     )
 }
 
